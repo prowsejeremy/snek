009732dcd800d74543c0e84e934160af898459dd
Jeremy Prowse <jeremyprowse@Jeremys-MacBook-Pro.local>
2023-07-27 21:26:53 +1200

    update fs package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const fs = require('fs')
+const fs = require('fs-extra')
 const bodyParser = require('body-parser')
 
 const app = module.exports = express()
